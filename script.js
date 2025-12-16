c80c72d8e52f3b276b107fabc671db0071e04715
E.santhosh <146980965+Santhosh14-git@users.noreply.github.com>
2025-12-16 10:13:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const codeText = `
+
+import { build, deploy } from "nexvion-core";
+
+const services = [
+  "Web Development",
+  "UI/UX Design",
+  "Hosting",
+  "SEO Optimization"
+];
+
+function startProject(client) {
+  if (!client.idea) return "Define Requirements";
+  build(client.idea);
+  deploy("production");
+}
+
+startProject("Startup");
+console.log("Website Live 🚀");
+`;
+
+const codeElement = document.getElementById("code");
+let index = 0;
+
+function typeCode() {
+  if (index < codeText.length) {
+    codeElement.textContent += codeText.charAt(index);
+    index++;
+    setTimeout(typeCode, 40);
+  } else {
+    setTimeout(() => {
+      codeElement.textContent = "";
+      index = 0;
+      typeCode();
+    }, 2000);
+  }
+}
+
+typeCode();
+
+
+
